703787afeadace4e3a9786b7cb1ac517d362ec76
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2025-10-25 15:34:28 +0700

    some update

--- src/components/Sidebar/SidebarMobile.tsx
+++ src/components/Sidebar/SidebarMobile.tsx
@@ -6,20 +6,27 @@ import SidebarMenu from './SidebarMenu';
 import SidebarToggle from './SidebarToggle';
 
 export default function SidebarMobile() {
-  const { mobileOpen } = useSidebarContext();
+  const { mobileOpen, toggleMobile } = useSidebarContext();
 
   return (
-    <div
-      data-show={mobileOpen}
-      className="absolute z-50 h-screen w-[90%] xs:w-[65%] sm:w-[45%] translate-x-[-100%] data-[show=true]:translate-x-0 transition-all duration-500"
-    >
-      <nav className="relative bg-background h-full rounded-r-[10px] border p-5 pb-3 transition-all duration-500">
-        <div className="flex justify-between">
-          <SidebarHeader />
-          <SidebarToggle />
-        </div>
-        <SidebarMenu />
-      </nav>
-    </div>
+    <>
+      <div
+        data-show={mobileOpen}
+        onClick={toggleMobile}
+        className="absolute hidden z-[49] w-full h-full data-[show=true]:block"
+      />
+      <div
+        data-show={mobileOpen}
+        className="absolute z-50 h-screen w-[90%] xs:w-[65%] sm:w-[45%] translate-x-[-100%] data-[show=true]:translate-x-0 transition-all duration-500"
+      >
+        <nav className="relative bg-background h-full rounded-r-[10px] border p-5 pb-3 transition-all duration-500">
+          <div className="flex justify-between">
+            <SidebarHeader />
+            <SidebarToggle />
+          </div>
+          <SidebarMenu />
+        </nav>
+      </div>
+    </>
   );
 }
