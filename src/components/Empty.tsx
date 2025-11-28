fd9bf1db6ae8d3c389b3f89687c479d513ac9886
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2025-11-28 21:06:20 +0700

    add empty state on chatroom

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,10 +1,23 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 export default function Empty(props: { title: string; description: string }) {
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <Image
-        src="/images/empty.png"
+        src={`/images/empty-${resolvedTheme}.png`}
         alt="Empty-Image"
         width={105.8}
         height={105.8}
--- src/modules/Chatroom/index.tsx
+++ src/modules/Chatroom/index.tsx
@@ -1,3 +1,4 @@
+import Empty from '@/components/Empty';
 import PageHeader from '@/components/PageHeader';
 
 export default function Chatroom() {
@@ -8,6 +9,12 @@ export default function Chatroom() {
         description={'A fun place to share your thoughts, give suggestions, and chat casually 👋'}
         hideCvButton
       />
+      <div className="h-[78vh] w-full ">
+        <Empty
+          title={'Oops! This feature is not available yet'}
+          description={'Stay tuned — something new is coming soon.'}
+        />
+      </div>
     </>
   );
 }
